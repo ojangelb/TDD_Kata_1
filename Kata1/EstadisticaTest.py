3fea59e5204a62c72dccb79e5a4a277175614471
dvergel <dvergel@amantium.net>
2020-03-15 13:07:22 -0500

    Ciclo 3: Funcionalidad con 2 numeros de entrada (Rojo)

--- Kata1/EstadisticaTest.py
+++ Kata1/EstadisticaTest.py
@@ -6,7 +6,11 @@ from Estadistica import Estadistica
 class EstadisticaTest(TestCase):
     def test_calculo_estadistica(self):
         self.assertEqual(Estadistica.calculoEstadistica(""), 0, "Vacio")
-        
+
     def test_calculo_estadistica_un_numero(self):
-        array = [1,1,1,1]
+        array = [1, 1, 1, 1]
         self.assertEqual(Estadistica.calculoEstadistica("1"), array, "Un número")
+
+    def test_calculo_estadistica_dos_numeros(self):
+        array = [2, 1, 2, 1.5]
+        self.assertEqual(Estadistica.calculoEstadistica("1,2"), array, "Un número")
