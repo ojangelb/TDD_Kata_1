777a08bdc8ae075be7fff04b8d6044b7d06be920
Oscar Javier Angel B <ojangelb@gmail.com>
2020-03-15 13:00:53 -0500

    Ciclo 2 - Refactor - funcionalidad un numero - (Azul)

--- Kata1/Estadistica.py
+++ Kata1/Estadistica.py
@@ -2,11 +2,20 @@
 class Estadistica:
     def calculoEstadistica(cadena):
         arrayResult = []
+        arrayString = []
+        arrayString = cadena.split(",")
+
+        try:
+            arrayString = [int(i) for i in arrayString]
+
+        except:
+             return 0
+
         if not cadena:
             return 0
         else:
-            if len(cadena)==1:
+            if len(arrayString):
 
-               arrayResult = [int(cadena),int(cadena),1,int(cadena)]
+               arrayResult = [arrayString[0],arrayString[0],1,arrayString[0]]
 
                return arrayResult
