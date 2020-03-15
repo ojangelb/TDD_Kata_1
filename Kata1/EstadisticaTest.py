f526b8e9b9ae0360028374e03f53918e2b1e248b
Oscar Javier Angel B <ojangelb@gmail.com>
2020-03-15 11:25:46 -0500

    Ciclo 1 - Creacion prueba un numero (Rojo)

--- Kata1/EstadisticaTest.py
+++ Kata1/EstadisticaTest.py
@@ -6,3 +6,6 @@ from Estadistica import Estadistica
 class EstadisticaTest(TestCase):
     def test_calculo_estadistica(self):
         self.assertEqual(Estadistica.calculoEstadistica(""), 0, "Vacio")
+        
+    def test_calculo_estadistica_un_numero(self):
+        self.assertEqual(Estadistica.calculoEstadistica("1"), 1, "Un número")
